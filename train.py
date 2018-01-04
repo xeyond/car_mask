17c2be8936c1940c55c1b74cfd7ec49a519a0693
wangxiyang <ssdutwxy@foxmail.com>
2018-01-04 21:35:10 +0800

    Remove keras api, use tf api instead

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ image_size = [1280 // 2, 1920 // 2]
 unet = Unet(input_shape=image_size)
 unet.build_net()
 X, Y = read_data('/home/wangxiyang/dataset/kaggle/data/train', '/home/wangxiyang/dataset/kaggle/data/train_masks',
-                 n_images=0, image_size=image_size)
+                 n_images=100, image_size=image_size)
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 1.0
 with tf.Session() as sess:
-    unet.train(sess, X, Y, epochs=1000, batch_size=1, learning_rate=0.0001)
+    unet.train(sess, X, Y, epochs=1000, batch_size=1, learning_rate=0.0005)
--- unet.py
+++ unet.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import sys
 from scipy import misc
 
+
 class Unet():
     def __init__(self, input_shape=(1280, 1920)):
         self.height, self.width = input_shape
@@ -11,6 +12,8 @@ class Unet():
         self.output_holder = tf.placeholder(tf.float32, shape=[None, self.height, self.width], name='output_holder')
 
         filter_num = 44
+
+        # left layers
         conv1_1 = tf.layers.conv2d(self.input_holder, filters=filter_num, kernel_size=(3, 3), activation=tf.nn.relu,
                                    padding='same')
         conv1_2 = tf.layers.conv2d(conv1_1, filters=filter_num, kernel_size=(3, 3), activation=tf.nn.relu,
@@ -35,73 +38,73 @@ class Unet():
                                    padding='same')
         max_pooling4 = tf.layers.max_pooling2d(conv4_2, pool_size=(2, 2), strides=(2, 2))
 
+        # center layers
         center_layer = tf.layers.conv2d(max_pooling4, filters=filter_num * 16, kernel_size=(3, 3),
                                         activation=tf.nn.relu,
                                         padding='same')
         center_layer = tf.layers.conv2d(center_layer, filters=filter_num * 16, kernel_size=(3, 3),
                                         activation=tf.nn.relu,
                                         padding='same')
-        up_sampling4 = tf.keras.layers.UpSampling2D((2, 2))(center_layer)
-        up_sampling4 = tf.layers.conv2d(up_sampling4, filters=filter_num * 8, kernel_size=(3, 3),
-                                        activation=tf.nn.relu,
-                                        padding='same')
-        up_sampling4 = tf.concat([conv4_2, up_sampling4], axis=-1)
-        up_conv4_1 = tf.layers.conv2d(up_sampling4, filters=filter_num * 8, kernel_size=(3, 3),
+
+        # right layers
+        up_conv4 = tf.layers.conv2d_transpose(center_layer, filters=filter_num * 8, kernel_size=(3, 3), strides=(2, 2),
+                                              activation=tf.nn.relu,
+                                              padding='same')
+        up_conv4 = tf.concat([conv4_2, up_conv4], axis=-1)
+        up_conv4_1 = tf.layers.conv2d(up_conv4, filters=filter_num * 8, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
         up_conv4_2 = tf.layers.conv2d(up_conv4_1, filters=filter_num * 8, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
 
-        up_sampling3 = tf.keras.layers.UpSampling2D((2, 2))(up_conv4_2)
-        up_sampling3 = tf.layers.conv2d(up_sampling3, filters=filter_num * 4, kernel_size=(3, 3),
-                                        activation=tf.nn.relu,
-                                        padding='same')
-        up_sampling3 = tf.concat([conv3_2, up_sampling3], axis=-1)
-        up_conv3_1 = tf.layers.conv2d(up_sampling3, filters=filter_num * 4, kernel_size=(3, 3),
+        up_conv3 = tf.layers.conv2d_transpose(up_conv4_2, filters=filter_num * 4, kernel_size=(3, 3), strides=(2, 2),
+                                              activation=tf.nn.relu,
+                                              padding='same')
+        up_conv3 = tf.concat([conv3_2, up_conv3], axis=-1)
+        up_conv3_1 = tf.layers.conv2d(up_conv3, filters=filter_num * 4, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
         up_conv3_2 = tf.layers.conv2d(up_conv3_1, filters=filter_num * 4, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
 
-        up_sampling2 = tf.keras.layers.UpSampling2D((2, 2))(up_conv3_2)
-        up_sampling2 = tf.layers.conv2d(up_sampling2, filters=filter_num * 2, kernel_size=(3, 3),
-                                        activation=tf.nn.relu,
-                                        padding='same')
-        up_sampling2 = tf.concat([conv2_2, up_sampling2], axis=-1)
-        up_conv2_1 = tf.layers.conv2d(up_sampling2, filters=filter_num * 2, kernel_size=(3, 3),
+        up_conv2 = tf.layers.conv2d_transpose(up_conv3_2, filters=filter_num * 2, kernel_size=(3, 3), strides=(2, 2),
+                                              activation=tf.nn.relu,
+                                              padding='same')
+        up_conv2 = tf.concat([conv2_2, up_conv2], axis=-1)
+
+        up_conv2_1 = tf.layers.conv2d(up_conv2, filters=filter_num * 2, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
         up_conv2_2 = tf.layers.conv2d(up_conv2_1, filters=filter_num * 2, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
 
-        up_sampling1 = tf.keras.layers.UpSampling2D((2, 2))(up_conv2_2)
-        up_sampling1 = tf.layers.conv2d(up_sampling1, filters=filter_num, kernel_size=(3, 3),
-                                        activation=tf.nn.relu,
-                                        padding='same')
-        up_sampling1 = tf.concat([conv1_2, up_sampling1], axis=-1)
-        up_conv1_1 = tf.layers.conv2d(up_sampling1, filters=filter_num, kernel_size=(3, 3),
+        up_conv1 = tf.layers.conv2d_transpose(up_conv2_2, filters=filter_num, kernel_size=(3, 3), strides=(2, 2),
+                                              activation=tf.nn.relu,
+                                              padding='same')
+        up_conv1 = tf.concat([conv1_2, up_conv1], axis=-1)
+        up_conv1_1 = tf.layers.conv2d(up_conv1, filters=filter_num, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
         up_conv1_2 = tf.layers.conv2d(up_conv1_1, filters=filter_num, kernel_size=(3, 3),
                                       activation=tf.nn.relu,
                                       padding='same')
-        # print(up_conv1_2)
 
-        mask_layer = tf.layers.conv2d(up_conv1_2, filters=1, kernel_size=(1, 1), activation=tf.nn.sigmoid,
-                                      padding='same')
-        mask_layer = tf.squeeze(mask_layer, axis=-1)
+        # output layer
+        mask_layer_logits = tf.layers.conv2d(up_conv1_2, filters=1, kernel_size=(1, 1), activation=None,
+                                             padding='same')
+        mask_layer_logits = tf.squeeze(mask_layer_logits, axis=-1)
+        mask_layer = tf.nn.sigmoid(mask_layer_logits)
         self.output_mask = mask_layer
         print(mask_layer)
-        self.loss = tf.reduce_mean(tf.keras.losses.binary_crossentropy(mask_layer, self.output_holder))
+        self.loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=mask_layer_logits, labels=self.output_holder))
         print(self.loss)
 
-
     def predict(self, sess, inputs):
-        return sess.run(self.output_mask, feed_dict={self.input_holder:inputs})
-    
+        return sess.run(self.output_mask, feed_dict={self.input_holder: inputs})
 
     def train(self, sess, inputs, outputs, batch_size=1, epochs=100, learning_rate=0.001):
         assert hasattr(self, 'loss')
@@ -120,7 +123,7 @@ class Unet():
                                                                 self.output_holder: output_batch})
                 sys.stdout.write("\repoch:%3d, idx: %4d, loss: %0.6f" % (epoch, idx, batch_loss))
                 total_loss += batch_loss
-                #misc.imsave('./test_result/train_%03d_epoch%03d.jpg' % (idx, epoch),output_map.reshape([self.height, self.width]))
+                # misc.imsave('./test_result/train_%03d_epoch%03d.jpg' % (idx, epoch),output_map.reshape([self.height, self.width]))
             print("\nepoch %3d, average loss: %.6f" % (epoch, total_loss / n_batches))
             if (epoch + 1) % 1 == 0:
                 saver.save(sess,
